42efe52535e71fc016097e33eb4c62b9aeec3f39
Edgar <harove@gmail.com>
2026-09-09 02:38:45 -0300

    testing update

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use("/", consoleLog, webRouter);
 app.use(manejoDeErrores);
 
 app.use((req, res, next) => {
-  res.status(404).send("Not Found");
+  res.status(404).send("Not Found"); //test
 });
 
 webSocketServer.on("connection", async (socket) => {
